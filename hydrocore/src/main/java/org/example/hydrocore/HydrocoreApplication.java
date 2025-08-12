3a6ff640df66bb8ba45a0b3536539a9f34c0e507
leonardolinsz <leonardo.furlanis@germinare.org.br>
2025-08-12 07:52:15 -0300

    Adicionando estrutura do repositório

---
+++ hydrocore/src/main/java/org/example/hydrocore/HydrocoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.hydrocore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydrocoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydrocoreApplication.class, args);
+	}
+
+}
---
+++ hydrocore/src/test/java/org/example/hydrocore/HydrocoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hydrocore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydrocoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
